71f1915d085ef1fcc9fcaf5812d85b39e9fe59b5
Michael Collison <collison956@gmail.com>
2023-01-05 01:05:52 -0500

    Fix partial calendar days

--- libs/timeserieslib/SyntheticTimeSeriesCreator.h
+++ libs/timeserieslib/SyntheticTimeSeriesCreator.h
@@ -5,8 +5,8 @@
 #include "TimeSeries.h"
 #include "McptConfigurationFileReader.h"
 #include "TimeSeriesCsvReader.h"
-
 #include <map>
+#include <cassert>
 
 using boost::posix_time::time_duration;
 
@@ -27,44 +27,80 @@ namespace mkc_timeseries
 
             void createSyntheticTimeSeries(int timeFrameId, time_duration filterTime) 
             {
-                std::shared_ptr<OHLCTimeSeries<Decimal>> syntheticTimeSeries = std::make_shared<OHLCTimeSeries<Decimal>>(
-                    TimeFrame::DAILY, 
-                    mOriginalHourlyTimeSeries->getVolumeUnits()
-                );
+	      std::shared_ptr<OHLCTimeSeries<Decimal>> syntheticTimeSeries = std::make_shared<OHLCTimeSeries<Decimal>>(
+														       TimeFrame::DAILY,
+														       mOriginalHourlyTimeSeries->getVolumeUnits()
+														       );
 
-                for(auto it = mOriginalHourlyTimeSeries->beginRandomAccess(); it != mOriginalHourlyTimeSeries->endRandomAccess(); it++)
+	      boost::gregorian::date dailyDate(mOriginalHourlyTimeSeries->getFirstDate());
+	      boost::gregorian::date intradayDate(dailyDate);
+	      time_duration partialDayFilterTime;
+	      bool partialDayFound = false;
+
+	      for(auto it = mOriginalHourlyTimeSeries->beginRandomAccess(); it != mOriginalHourlyTimeSeries->endRandomAccess(); it++)
                 {
-                    // this 'if' prevents the last bar for the first time value from being added to the time series
-                    // since it won't be added until the next date is found, i.e. if the file ends on 12/29/2020
-                    // the 12/29/2020 9:00 bar will be missing since it won't be added until the values are aggregated
-                    // and the 9:00 timestamp is found again on 12/30/2020.
-                    if(filterTime == it->getBarTime()) 
+		  intradayDate = it->getDateValue();
+		  if (intradayDate != dailyDate)
+		    {
+		      dailyDate = intradayDate;
+		      //Check for partial days
+		      boost::posix_time::ptime newDateWithTimeFilter(intradayDate, filterTime);
+
+		      typename OHLCTimeSeries<Decimal>::ConstTimeSeriesIterator it2 = 
+			mOriginalHourlyTimeSeries->getTimeSeriesEntry(newDateWithTimeFilter);
+
+		      if (it2 == mOriginalHourlyTimeSeries->endSortedAccess())
+			{
+			  // We found a partial day
+			  partialDayFilterTime = getLastTimeForPartialDay(it, mOriginalHourlyTimeSeries->endRandomAccess(), intradayDate);
+			  partialDayFound = true;
+			}
+
+		    }
+		  // this 'if' prevents the last bar for the first time value from being added to the time series
+		  // since it won't be added until the next date is found, i.e. if the file ends on 12/29/2020
+		  // the 12/29/2020 9:00 bar will be missing since it won't be added until the values are aggregated
+		  // and the 9:00 timestamp is found again on 12/30/2020.
+
+		  if(filterTime == it->getBarTime() || ((partialDayFound == true) && (partialDayFilterTime == it->getBarTime())))
                     {
-                        if(mOpen != DecimalConstants<Decimal>::DecimalZero)
+		      if(mOpen != DecimalConstants<Decimal>::DecimalZero)
                         {
-                            syntheticTimeSeries->addEntry(OHLCTimeSeriesEntry<Decimal>(
-                                mEntryDate, mOpen, mHigh, mLow, mClose, it->getVolumeValue(), TimeFrame::DAILY));
+			  syntheticTimeSeries->addEntry(OHLCTimeSeriesEntry<Decimal>(mEntryDate,
+										     mOpen,
+										     mHigh,
+										     mLow,
+										     mClose,
+										     it->getVolumeValue(),
+										     TimeFrame::DAILY));
                         }
-                        mOpen = it->getOpenValue();
-                        mHigh = DecimalConstants<Decimal>::DecimalZero;
-                        mLow = it->getHighValue() * DecimalConstants<Decimal>::DecimalOneHundred;
-                        mEntryDate = it->getDateValue();
+
+		      if (!partialDayFound)
+			mOpen = it->getOpenValue();
+		      else
+			mOpen = it->getCloseValue();
+
+		      mHigh = DecimalConstants<Decimal>::DecimalZero;
+		      mLow = it->getHighValue() * DecimalConstants<Decimal>::DecimalOneHundred;
+		      mEntryDate = it->getDateValue();
+		      partialDayFound = false;
                     }
-                    if(it->getHighValue() > mHigh)
-                        mHigh = it->getHighValue();
-                    if(it->getLowValue() < mLow)
-                        mLow = it->getLowValue();
+
+		  if(it->getHighValue() > mHigh)
+		    mHigh = it->getHighValue();
+		  if(it->getLowValue() < mLow)
+		    mLow = it->getLowValue();
                     
-                    mClose = it->getCloseValue();
+		  mClose = it->getCloseValue();
                 }
 
-                // need to add the missing bar for the first time frame, the aggregated values should be set
-                // correctly for this bar -- tested this on KC and MSFT data to make sure
-                if(timeFrameId == 1)
-                    syntheticTimeSeries->addEntry(OHLCTimeSeriesEntry<Decimal>(
-                        mEntryDate, mOpen, mHigh, mLow, mClose, mOriginalHourlyTimeSeries->getVolumeValue(mOriginalHourlyTimeSeries->beginRandomAccess(), 0), TimeFrame::DAILY));
+	      // need to add the missing bar for the first time frame, the aggregated values should be set
+	      // correctly for this bar -- tested this on KC and MSFT data to make sure
+	      if(timeFrameId == 1)
+		syntheticTimeSeries->addEntry(OHLCTimeSeriesEntry<Decimal>(
+									   mEntryDate, mOpen, mHigh, mLow, mClose, mOriginalHourlyTimeSeries->getVolumeValue(mOriginalHourlyTimeSeries->beginRandomAccess(), 0), TimeFrame::DAILY));
 
-                mTimeSeriesMap.insert(std::make_pair(timeFrameId, syntheticTimeSeries));
+	      mTimeSeriesMap.insert(std::make_pair(timeFrameId, syntheticTimeSeries));
             }
             
             std::shared_ptr<OHLCTimeSeries<Decimal>> getSyntheticTimeSeries(int timeFrameId)
@@ -92,6 +128,30 @@ namespace mkc_timeseries
             Decimal mLow = DecimalConstants<Decimal>::DecimalZero;
             Decimal mClose = DecimalConstants<Decimal>::DecimalZero;
             boost::gregorian::date mEntryDate;
+
+            time_duration getLastTimeForPartialDay(typename OHLCTimeSeries<Decimal>::RandomAccessIterator partialDayIt,
+                                                   typename OHLCTimeSeries<Decimal>::RandomAccessIterator endIt,
+                                                    boost::gregorian::date intradayDate)
+            {
+                boost::gregorian::date currentDate(intradayDate);
+                time_duration lastTimeStamp;
+
+                assert(partialDayIt != endIt);
+                assert(partialDayIt->getDateValue() == currentDate);
+
+                while (partialDayIt != endIt)
+                {
+                    if (partialDayIt->getDateValue() != currentDate)
+                    {
+                        return lastTimeStamp;
+                    }
+                    else
+                    {
+                        lastTimeStamp = partialDayIt->getBarTime();
+                        partialDayIt++;
+                    }
+                }
+            }
 
             std::string getTimeFrameFilename(int timeFrameId) 
             {
