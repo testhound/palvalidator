d202d4d9f88e164a375ddba3bcbc4f6750fdb4a7
Michael Collison <collison956@gmail.com>
2023-01-05 01:08:12 -0500

    New class to handle gory details of created mixed data files

---
+++ libs/timeserieslib/TimeShiftedMultiTimeSeriesCreator.h
@@ -0,0 +1,113 @@
+#ifndef TIME_SHIFTED_MULTI_TIME_SERIES_CREATOR_H
+#define TIME_SHIFTED_MULTI_TIME_SERIES_CREATOR_H
+
+#include "SyntheticTimeSeriesCreator.h"
+#include "TimeFrameDiscovery.h"
+#include "TimeSeries.h"
+#include "TimeSeriesCsvReader.h"
+#include "Security.h"
+#include "TimeSeriesValidator.h"
+
+namespace mkc_timeseries
+{
+  template <class Decimal>
+  class TimeShiftedMultiTimeSeriesCreator
+  {
+  public:
+    typedef typename std::vector<std::shared_ptr<OHLCTimeSeries<Decimal>>> ShiftedTimeSeriesContainer;
+    typedef typename ShiftedTimeSeriesContainer::const_iterator ShiftedTimeSeriesIterator;
+
+    TimeShiftedMultiTimeSeriesCreator(std::shared_ptr<Security<Decimal>> security) :
+      mShiftedTimeSeriesContainer(),
+      mSecurity(security)
+    {}
+      
+    virtual ~TimeShiftedMultiTimeSeriesCreator()
+    {}
+
+    virtual void createShiftedTimeSeries() = 0;
+
+    ShiftedTimeSeriesIterator beginShiftedTimeSeries() const
+    {
+      return mShiftedTimeSeriesContainer.begin();
+    }
+
+    ShiftedTimeSeriesIterator endShiftedTimeSeries() const
+    {
+      return mShiftedTimeSeriesContainer.end();
+    }
+    
+    int numTimeSeriesCreated() const
+      {
+	return mShiftedTimeSeriesContainer.size();
+      }
+
+  protected:
+    std::shared_ptr<Security<Decimal>> getSecurity() const
+      {
+	return mSecurity;
+      }
+
+    void addTimeSeries(std::shared_ptr<OHLCTimeSeries<Decimal>> series)
+      {
+	mShiftedTimeSeriesContainer.push_back(series);
+      }
+
+  private:
+    ShiftedTimeSeriesContainer mShiftedTimeSeriesContainer;
+    std::shared_ptr<Security<Decimal>> mSecurity;
+};
+
+  // This class creates multiple, time shifted end of day time series from
+  // a input intraday time series.
+
+  template <class Decimal>
+  class DailyTimeShiftedMultiTimeSeriesCreator : public TimeShiftedMultiTimeSeriesCreator<Decimal>
+  {
+  public:
+    DailyTimeShiftedMultiTimeSeriesCreator(const std::string intradayDataFilePath, const std::shared_ptr<Security<Decimal>> security)
+      : TimeShiftedMultiTimeSeriesCreator<Decimal>(security),
+      mIntradayDataFilePath(intradayDataFilePath)
+    {}
+
+    void createShiftedTimeSeries()
+      {
+	std::shared_ptr<TimeSeriesCsvReader<Decimal>> reader = 
+	  std::make_shared<TradeStationFormatCsvReader<Decimal>>(mIntradayDataFilePath,
+								 TimeFrame::INTRADAY,
+								 this->getSecurity()->getVolumeUnit(),
+								 this->getSecurity()->getTick());
+        reader->readFile();
+
+        std::shared_ptr<TimeFrameDiscovery<Decimal>> timeFrameDiscovery = 
+	  std::make_shared<TimeFrameDiscovery<Decimal>>(reader->getTimeSeries());
+
+        timeFrameDiscovery->inferTimeFrames();
+
+        std::shared_ptr<SyntheticTimeSeriesCreator<Decimal>> syntheticTimeSeriesCreator = 
+          std::make_shared<SyntheticTimeSeriesCreator<Decimal>>(reader->getTimeSeries(), mIntradayDataFilePath);
+
+        std::shared_ptr<TimeSeriesValidator<Decimal>> validator = 
+	  std::make_shared<TimeSeriesValidator<Decimal>>(reader->getTimeSeries(),
+							 this->getSecurity()->getTimeSeries(),
+							 timeFrameDiscovery->numTimeFrames());
+        validator->validate();
+
+	int timeFrameId = 0;
+        for(int i = 0; i < timeFrameDiscovery->numTimeFrames(); i++) 
+        {
+	  timeFrameId = i + 1;
+          boost::posix_time::time_duration timeStamp = timeFrameDiscovery->getTimeFrame(i);
+          syntheticTimeSeriesCreator->createSyntheticTimeSeries(timeFrameId, timeStamp);
+
+	  this->addTimeSeries (syntheticTimeSeriesCreator->getSyntheticTimeSeries(timeFrameId));
+          syntheticTimeSeriesCreator->writeTimeFrameFile(timeFrameId);
+        }
+      }
+
+  private:
+    std::string mIntradayDataFilePath;
+  };
+}
+
+#endif
---
+++ libs/timeserieslib/test/TimeShiftedMultiTimeSeriesCreatorTest.cpp
@@ -0,0 +1,106 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "TimeShiftedMultiTimeSeriesCreator.h"
+#include "DecimalConstants.h"
+#include "TestUtils.h"
+
+typedef dec::decimal<7> DecimalType;
+
+using namespace mkc_timeseries;
+using namespace boost::gregorian;
+using boost::posix_time::time_duration;
+
+void findMissingDatesBetweenSeries(std::shared_ptr<OHLCTimeSeries<DecimalType> >series1,
+				   std::shared_ptr<OHLCTimeSeries<DecimalType>> series2)
+{
+    auto outer_it = series1->beginSortedAccess();
+   // OHLCTimeSeriesEntry<DecimalType> entry1, entry2;
+    boost::gregorian::date entry1_date;
+    for(; outer_it != series1->endSortedAccess(); outer_it++)
+      {
+	auto entry1 = outer_it->second;
+	entry1_date = entry1.getDateValue();
+	if (series2->isDateFound(entry1_date) == false)
+	  std::cout << "date: " << entry1_date << "not found in series2" << std::endl;
+      }
+
+}
+
+TEST_CASE ("TimeShiftedMultiTimeSeriesCreator operations", "[DailyTimeShiftedMultiTimeSeriesCreator]")
+{
+  std::string equitySymbol("SSO");
+  std::string equityName("ProShares Ultra S&P500");
+
+  auto ssoDailyFile = std::make_shared<TradeStationFormatCsvReader<DecimalType>>("SSO_RAD_Daily.txt",
+										 TimeFrame::DAILY,
+										 TradingVolume::SHARES,
+										 DecimalConstants<DecimalType>::EquityTick);
+  ssoDailyFile->readFile();
+
+  auto ssoIntradayFile = std::make_shared<TradeStationFormatCsvReader<DecimalType>>("SSO_RAD_Hourly.txt",
+										    TimeFrame::INTRADAY,
+										    TradingVolume::SHARES,
+										    DecimalConstants<DecimalType>::EquityTick);
+ 
+  ssoIntradayFile->readFile();
+
+  auto ssoDaily = std::make_shared<EquitySecurity<DecimalType>>(equitySymbol, equityName, ssoDailyFile->getTimeSeries());
+  auto ssoHourly = std::make_shared<EquitySecurity<DecimalType>>(equitySymbol, equityName, ssoIntradayFile->getTimeSeries());
+
+  auto timeShiftedCreator = std::make_shared<DailyTimeShiftedMultiTimeSeriesCreator<DecimalType>>("SSO_RAD_Hourly.txt",
+												  ssoDaily);
+  timeShiftedCreator->createShiftedTimeSeries();
+
+  SECTION ("DailyTimeShiftedMultiTimeSeriesCreator", "[DailyTimeShiftedMultiTimeSeriesCreator]")
+  {
+    REQUIRE(timeShiftedCreator->numTimeSeriesCreated() == 7);
+
+    std::shared_ptr<OHLCTimeSeries<Decimal>> outerTimeSeries;
+    std::shared_ptr<OHLCTimeSeries<Decimal>> innerTimeSeries;
+
+    auto outer_it = timeShiftedCreator->beginShiftedTimeSeries();
+    int outer_time_series_num = 0;
+    int inner_time_series_num = 0;
+
+    // Make sure the generated time series are the same in number of elements, etc but
+    // different in the actual elements
+    for(; outer_it != timeShiftedCreator->endShiftedTimeSeries(); outer_it++)
+      {
+	outer_time_series_num++;
+	outerTimeSeries = *outer_it;
+	auto inner_it = outer_it + 1;
+	inner_time_series_num = outer_time_series_num;
+
+	for(; inner_it != timeShiftedCreator->endShiftedTimeSeries(); inner_it++)
+	  {
+	    inner_time_series_num++;
+	    innerTimeSeries = *inner_it;
+
+	    REQUIRE(outerTimeSeries->getTimeFrame() == innerTimeSeries->getTimeFrame());
+	    if (outerTimeSeries->getNumEntries() != innerTimeSeries->getNumEntries())
+	      findMissingDatesBetweenSeries(outerTimeSeries, innerTimeSeries);
+	    
+	    REQUIRE(outerTimeSeries->getNumEntries() == innerTimeSeries->getNumEntries());
+	    REQUIRE(outerTimeSeries->getVolumeUnits() == innerTimeSeries->getVolumeUnits());
+	    REQUIRE(outerTimeSeries->getFirstDate() == innerTimeSeries->getFirstDate());
+	    REQUIRE(outerTimeSeries->getLastDate() == innerTimeSeries->getLastDate());
+	    REQUIRE(outerTimeSeries != innerTimeSeries);
+	  }
+      }
+
+    outer_it = timeShiftedCreator->beginShiftedTimeSeries();
+    auto ssoDailyTimeSeries = ssoDaily->getTimeSeries();
+
+    for(; outer_it != timeShiftedCreator->endShiftedTimeSeries(); outer_it++)
+      {
+	outerTimeSeries = *outer_it;
+	REQUIRE(outerTimeSeries->getTimeFrame() == ssoDailyTimeSeries->getTimeFrame());
+	REQUIRE(outerTimeSeries->getNumEntries() == ssoDailyTimeSeries->getNumEntries());
+	REQUIRE(outerTimeSeries->getVolumeUnits() == ssoDailyTimeSeries->getVolumeUnits());
+	REQUIRE(outerTimeSeries->getFirstDate() == ssoDailyTimeSeries->getFirstDate());
+	REQUIRE(outerTimeSeries->getLastDate() == ssoDailyTimeSeries->getLastDate());
+	REQUIRE(outerTimeSeries != ssoDailyTimeSeries);
+      }
+  }
+}
