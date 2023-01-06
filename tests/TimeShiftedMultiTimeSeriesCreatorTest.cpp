0d1cf1c07fb1c96184027f1df4f134a83527960c
Michael Collison <collison956@gmail.com>
2023-01-06 18:24:26 -0500

    Add complete test case

--- tests/TimeShiftedMultiTimeSeriesCreatorTest.cpp
+++ tests/TimeShiftedMultiTimeSeriesCreatorTest.cpp
@@ -11,6 +11,22 @@ using namespace mkc_timeseries;
 using namespace boost::gregorian;
 using boost::posix_time::time_duration;
 
+void findMissingDatesBetweenSeries(std::shared_ptr<OHLCTimeSeries<DecimalType> >series1,
+				   std::shared_ptr<OHLCTimeSeries<DecimalType>> series2)
+{
+    auto outer_it = series1->beginSortedAccess();
+   // OHLCTimeSeriesEntry<DecimalType> entry1, entry2;
+    boost::gregorian::date entry1_date;
+    for(; outer_it != series1->endSortedAccess(); outer_it++)
+      {
+	auto entry1 = outer_it->second;
+	entry1_date = entry1.getDateValue();
+	if (series2->isDateFound(entry1_date) == false)
+	  std::cout << "date: " << entry1_date << "not found in series2" << std::endl;
+      }
+
+}
+
 TEST_CASE ("TimeShiftedMultiTimeSeriesCreator operations", "[DailyTimeShiftedMultiTimeSeriesCreator]")
 {
   std::string equitySymbol("SSO");
@@ -44,16 +60,47 @@ TEST_CASE ("TimeShiftedMultiTimeSeriesCreator operations", "[DailyTimeShiftedMul
     std::shared_ptr<OHLCTimeSeries<Decimal>> innerTimeSeries;
 
     auto outer_it = timeShiftedCreator->beginShiftedTimeSeries();
-    for(; outer_it != timeShiftedCreator->endShiftedTimeSeries; outer_it++)
-      {
-	outerTimeSeries = *outer_it;
+    int outer_time_series_num = 0;
+    int inner_time_series_num = 0;
 
+    // Make sure the generated time series are the same in number of elements, etc but
+    // different in the actual elements
+    for(; outer_it != timeShiftedCreator->endShiftedTimeSeries(); outer_it++)
+      {
+	outer_time_series_num++;
+	outerTimeSeries = *outer_it;
 	auto inner_it = outer_it + 1;
-	for(; inner_it != timeShiftedCreator->endShiftedTimeSeries; inner_it++)
+	inner_time_series_num = outer_time_series_num;
+
+	for(; inner_it != timeShiftedCreator->endShiftedTimeSeries(); inner_it++)
 	  {
+	    inner_time_series_num++;
 	    innerTimeSeries = *inner_it;
+
+	    REQUIRE(outerTimeSeries->getTimeFrame() == innerTimeSeries->getTimeFrame());
+	    if (outerTimeSeries->getNumEntries() != innerTimeSeries->getNumEntries())
+	      findMissingDatesBetweenSeries(outerTimeSeries, innerTimeSeries);
+	    
+	    REQUIRE(outerTimeSeries->getNumEntries() == innerTimeSeries->getNumEntries());
+	    REQUIRE(outerTimeSeries->getVolumeUnits() == innerTimeSeries->getVolumeUnits());
+	    REQUIRE(outerTimeSeries->getFirstDate() == innerTimeSeries->getFirstDate());
+	    REQUIRE(outerTimeSeries->getLastDate() == innerTimeSeries->getLastDate());
+	    REQUIRE(outerTimeSeries != innerTimeSeries);
 	  }
       }
 
+    outer_it = timeShiftedCreator->beginShiftedTimeSeries();
+    auto ssoDailyTimeSeries = ssoDaily->getTimeSeries();
+
+    for(; outer_it != timeShiftedCreator->endShiftedTimeSeries(); outer_it++)
+      {
+	outerTimeSeries = *outer_it;
+	REQUIRE(outerTimeSeries->getTimeFrame() == ssoDailyTimeSeries->getTimeFrame());
+	REQUIRE(outerTimeSeries->getNumEntries() == ssoDailyTimeSeries->getNumEntries());
+	REQUIRE(outerTimeSeries->getVolumeUnits() == ssoDailyTimeSeries->getVolumeUnits());
+	REQUIRE(outerTimeSeries->getFirstDate() == ssoDailyTimeSeries->getFirstDate());
+	REQUIRE(outerTimeSeries->getLastDate() == ssoDailyTimeSeries->getLastDate());
+	REQUIRE(outerTimeSeries != ssoDailyTimeSeries);
+      }
   }
 }
