9316648cf3986cbb247a72f2470cc47adc271f9a
Michael Collison <collison956@gmail.com>
2025-05-04 23:45:44 -0400

    add more unit tests that compiler

--- libs/timeserieslib/test/runtests.sh
+++ libs/timeserieslib/test/runtests.sh
@@ -1,4 +1,15 @@
 #!/bin/bash -x
+
+g++ -O2 -g -c -std=c++14 -I../ -I. -I/usr/local/include/priceactionlab -I/usr/local/include -DCPPTRACE_STATIC_DEFINE TestUtils.cpp
+
+g++ -O2 -g -c -std=c++14 -I../ -I. -I/usr/local/include/priceactionlab -I/usr/local/include -DCPPTRACE_STATIC_DEFINE TimeSeriesTest.cpp
+
+g++ -o TimeSeriesTest TimeSeriesTest.o TestUtils.o ../LogPalPattern.o ../TimeSeriesEntry.o ../TimeSeries.o ../BacktesterStrategy.o ../runner.o ./catch_amalgamated.o -L/usr/local/lib -lboost_date_time -lpriceaction2 -lboost_system -lboost_thread -lboost_filesystem -lCatch2 -pthread
+
+g++ -O2 -g -c -std=c++14 -I../ -I. -I/usr/local/include/priceactionlab -I/usr/local/include -DCPPTRACE_STATIC_DEFINE PalStrategyTest.cpp
+
+g++ -o PalStrategyTest PalStrategyTest.o TestUtils.o ../LogPalPattern.o ../TimeSeriesEntry.o ../TimeSeries.o ../BacktesterStrategy.o ../runner.o ./catch_amalgamated.o -L/usr/local/lib -lboost_date_time -lpriceaction2 -lboost_system -lboost_thread -lboost_filesystem -lCatch2 -pthread
+
 g++ -O2 -g -c -std=c++14 -I../ -I. -I/usr/local/include/priceactionlab -I/usr/local/include -DCPPTRACE_STATIC_DEFINE BackTesterTest.cpp
 g++ -o BackTesterTest BackTesterTest.o TestUtils.o ../LogPalPattern.o ../TimeSeriesEntry.o ../TimeSeries.o ../BacktesterStrategy.o ./catch_amalgamated.o -L/usr/local/lib -lboost_date_time -lpriceaction2 -lboost_system -lCatch2 -lcpptrace -ldwarf -lzstd -ldwarf -lz -lboost_filesystem -pthread
 
