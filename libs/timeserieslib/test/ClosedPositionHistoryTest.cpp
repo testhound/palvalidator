aae5111f13aabb817356587b54229c54b35635c2
Michael Collison <collison956@gmail.com>
2025-05-13 21:11:05 -0400

    Fix included statements

--- libs/timeserieslib/test/ClosedPositionHistoryTest.cpp
+++ libs/timeserieslib/test/ClosedPositionHistoryTest.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
-#include "../TimeSeriesCsvReader.h"
-#include "../ClosedPositionHistory.h"
-#include "../TimeSeriesIndicators.h"
+#include "TimeSeriesCsvReader.h"
+#include "ClosedPositionHistory.h"
+#include "TimeSeriesIndicators.h"
 #include "TestUtils.h"
 #include <cmath>
 
