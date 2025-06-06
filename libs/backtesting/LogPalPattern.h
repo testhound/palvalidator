418c7eb0cef510b606022617689b2b7617215be5
Michael Collison <collison956@gmail.com>
2025-06-06 19:22:30 -0400

    Modify LogPalPattern to only use shared_ptr, allow shared access to And_Expr lhs/rhs

--- libs/backtesting/LogPalPattern.h
+++ libs/backtesting/LogPalPattern.h
@@ -25,16 +25,16 @@ namespace mkc_timeseries
   private:
     static void LogPatternDescription (std::shared_ptr<PatternDescription> desc,
 				       std::ostream& outputFileStream);
-    static void LogExpression (PatternExpression *expression,
-			       std::ostream& outputFileStream);
-    static void LogPriceBarExpr (PriceBarReference *barReference,
-				 std::ostream& outputFileStream);
-    static void LogMarketExpression (MarketEntryExpression *expression,
-				     std::ostream& outputFileStream);
-    static void LogProfitTarget (ProfitTargetInPercentExpression *expression,
-				 std::ostream& outputFileStream);
-    static void LogStopLoss (StopLossInPercentExpression *expression,
-			     std::ostream& outputFileStream);
+    static void LogExpression (std::shared_ptr<PatternExpression> expression,
+          std::ostream& outputFileStream);
+    static void LogPriceBarExpr (std::shared_ptr<PriceBarReference> barReference,
+     std::ostream& outputFileStream);
+    static void LogMarketExpression (std::shared_ptr<MarketEntryExpression> expression,
+         std::ostream& outputFileStream);
+    static void LogProfitTarget (std::shared_ptr<ProfitTargetInPercentExpression> expression,
+     std::ostream& outputFileStream);
+    static void LogStopLoss (std::shared_ptr<StopLossInPercentExpression> expression,
+        std::ostream& outputFileStream);
     static void LogPatternSeparator(std::ostream& outputFileStream);
   };
 
