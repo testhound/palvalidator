dd002e771e84cd68e9501bdf3db9038b9ce78fed
Michael Collison <collison956@gmail.com>
2025-08-18 17:38:03 -0400

    Remove BackupScript.sh as it is no longer required

--- libs/timeseries/BackupScript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x #echo on
-
-cp *.cpp ~/Dropbox/TimeSeriesLibrary/.
-cp *.h ~/Dropbox/TimeSeriesLibrary/.
-cp *.hpp ~/Dropbox/TimeSeriesLibrary/.
-cp Makefile ~/Dropbox/TimeSeriesLibrary/.
-cp test/*.cpp ~/Dropbox/TimeSeriesLibrary/test/.
-cp test/Makefile ~/Dropbox/TimeSeriesLibrary/test/.
